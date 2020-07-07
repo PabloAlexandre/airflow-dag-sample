a48bd4db8d9a92e67fde7f4179ab6bee33072ec2
Pablo Alexandre <pabloalexandre.nave@gmail.com>
2020-07-07 01:27:08 -0300

    chore: add another dag

---
+++ oh-my-dag.py
@@ -0,0 +1,42 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+
+def sleep(time):
+  print(f"Sleeping {time} seconds")
+  time.sleep(random_base)
+
+def welcome():
+  print('Hello!')
+
+default_args = {
+    "owner": "Pablo",
+    "start_date": datetime(2020, 7, 6),
+    "retries": 3,
+    "retry_delay": timedelta(minutes=5),
+}
+
+dag = DAG(
+  'oh_my_dag',
+  default_args=default_args,
+  description='Another sample dag',
+  schedule_interval=timedelta(days=1),
+)
+
+hello = PythonOperator(
+    task_id='hello',
+    provide_context=True,
+    python_callable=welcome,
+    dag=dag,
+)
+
+for i in range(5):
+  task = PythonOperator(
+    task_id='sleep_for_' + str(i),
+    python_callable=sleep,
+    op_kwargs={'time': float(i) / 10},
+    dag=dag,
+  )
+
+  hello >> task
